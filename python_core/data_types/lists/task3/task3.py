64fc7ce112fc19db4c501060d0f5683417a23239
SzymonIwaniuk <s.f.iwaniuk@gmail.com>
2025-07-11 21:05:25 +0200

    feat: Add task solutions

---
+++ python_core/data_types/lists/task3/task3.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+def foo(nums: List[int]) -> List[int]:
+    produckt = 1
+    n = len(nums)
+
+    for multi in nums:
+        produckt *= multi
+
+    for i in range(n):
+        nums[i] = produckt // nums[i]
+
+    return nums
